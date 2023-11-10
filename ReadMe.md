790d47c6ac52d33c156fad30cb411c760e892c25
Eric Winebrenner <ejwinebrenner@gmail.com>
2023-11-10 12:27:30 -0500

    base notes added

---
+++ ReadMe.md
@@ -0,0 +1,27 @@
+# Notes
+There are two different branches associated with this lesson.
+- Main
+  - This branch provides the base notes of material not filled out.
+- Completed
+  - This branch includes all the added notes for you to review and compare with your own code-along.
+
+### Topics Covered
+- Promises
+- Resolvers
+- `setTimeout()`
+  - window method
+
+### What Should Already Be Understood
+- Classes, methods, objects, BOM
+
+## Files in this Lesson
+- `index.js`
+- `index.html`
+- assets
+  - `01_funcAsArgument.png`
+  - `02_dataFlow.png`
+
+## Resources
+- [MDN Docs Promises](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Using_promises)
+- [setTimeout()](https://www.w3schools.com/jsref/met_win_settimeout.asp)
+- [Resolvers](https://www.geeksforgeeks.org/javascript-promise-resolve-method/)
---
+++ index.js
@@ -0,0 +1,63 @@
+/* 
+    - Data will likely NOT be stored directly within our projects.
+        - Getting a list of users.
+        - Getting data per a users account.
+*/
+
+//? MOCK INTERNET REQUEST FUNCTION
+/* 
+*   Using this window method to mimic this request.
+    - setTimeout(cb,number)
+        - takes two parameters
+            - Callback function
+            - A number
+                - References a length of time in milliseconds
+                - ex: 5 seconds = 5000
+*/
+
+
+//! Promise
+/* 
+    A promise is an object that may produce a singular value in the return.
+
+    Three (3) possible states:
+        - Pending
+        - Fulfilled
+        - Rejected
+
+    Requires two parameters
+        - resolve
+        - reject
+
+    ALWAYS returns an object
+*/
+
+
+//! Resolvers
+/* 
+    - When returned, the returnData() function will return a Promise instead of a string.
+
+    - Chained with a resolver
+    - Takes in a callback function
+    - syntax notes: semicolo (;) placed ONLY after the last resolver
+
+*       Keywords:
+            .then(cb) 
+                - success
+                - can be chained together
+            .catch(cb)
+                - error
+
+*/
+
+
+//? The Process
+/* 
+    - Creating a new Promise
+        - resolve / reject are parameters of a callback function
+    - setTimeout() is acting out our "waiting" for a resource
+    - resolve() or reject() response
+    - When returned, returns a Promise - not a string
+    - use of resolvers (.then() / .catch()) with callbacks to extrapolate the info from that promise.
+
+ */
